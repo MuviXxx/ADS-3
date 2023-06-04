e6914b7cf07a28055109390582cdd1915f98abf3
MuviXxx <118690549+MuviXxx@users.noreply.github.com>
2023-06-04 22:34:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,27 @@
 // Copyright 2021 NNTU-CS
-
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int left = 0;
+    int right = size - 1;
+    int founded = 0;
+    while (right >= left) {
+        int mid = (right + left) / 2;
+        if (*(arr + mid) == value) {
+            founded += 1;
+            int newmid = mid;
+            while ((*(arr + mid + 1)) == value) {
+                founded += 1;
+                mid += 1;
+            }
+            while ((*(arr + newmid - 1)) == value) {
+                founded += 1;
+                newmid -= 1;
+            }
+            return founded;
+        } else if (*(arr + mid) > value) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    return founded;
 }
